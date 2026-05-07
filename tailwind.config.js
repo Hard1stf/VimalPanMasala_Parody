b0f2685428f9fa83b1e5ea312510c86b67c9edc0
Hard1stf <hardikvijeta2@gmail.com>
2026-05-07 14:27:30 +0000

    build(tailwind-config): update tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B91C1C",
+        secondary: "#F59E0B",
+        dark: "#111111",
+        light: "#FFFFFF",
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "1.5rem",
+          lg: "2rem",
+          xl: "3rem",
+        },
+      },
+
+      screens: {
+        xs: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+      },
+    },
   },
   plugins: [],
 }
