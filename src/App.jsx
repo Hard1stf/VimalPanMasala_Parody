9f44286ae49af48a982736de5eba9bf219ff2700
Hard1stf <hardikvijeta2@gmail.com>
2026-05-08 12:47:48 +0000

    feat(NavBar): Create a NavBar Compoenet
    Using Link component, and import in App.jsx and wrap in the wrapper component BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
+import { BrowserRouter,  } from 'react-router-dom';
 import './App.css';
+import NavBar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="p-10 space-y-4">
-      <div className="bg-red-500 text-white p-4 rounded">Red Box</div>
-
-      <div className="bg-[#123456] text-white p-4 rounded">Custom Color (JIT Test)</div>
-
-      <div className="text-sm md:text-3xl">Resize screen (Responsive Test)</div>
-
-      <button className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded transition">
-        Hover Me
-      </button>
-    </div>
+   <>
+   <BrowserRouter>
+      <NavBar />
+      <h1 className="text-3xl font-bold text-red-600">Vimal Pan Masala</h1>
+   </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return(
+        <>
+            <nav className="sticky top-0 right-0 z-50 max-w-7xl mx-auto p-4 flex justify-between item-center">
+                <Link to='/'>
+                    <h1 className="text-2xl font-extrabold text-orange-600 tracking-tighter">Vimal</h1>
+                </Link>
+                <div className="flex items-center gap-4">
+                    <Link to='#products' className="text-orange-950 font-bold tracking-tight hover:text-orange-900 transition-all duration-300">
+                        Products
+                    </Link>
+                    <button className="bg-orange-600 text-white font-medium px-4 py-2 hover:bg-orange-700 rounded-lg transition-all duration-300">
+                        Login
+                    </button>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default NavBar;
