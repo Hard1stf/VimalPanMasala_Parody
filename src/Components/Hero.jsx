ff1292679ad1ae10006456d52773429c173a521f
Hard1stf <hardikvijeta2@gmail.com>
2026-05-09 18:30:30 +0000

    feat(Hero): Create a hero section

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,27 @@
+
+
+const Hero = () => {
+    return(
+        <>
+            <section className="max-w-7xl mx-auto pt-12 px-4 flex flex-col items-center justify-center gap-y-8">
+            <div className="pt-8 flex flex-col items-center justify-center">
+                <h1 className="md:text-6xl text4xl font-extrabold text-orange-950 tracking-tighter">
+                    Bolo Zubaan <span className="text-orange-500">Canceri</span>
+                </h1>
+                <p className="md:text-xl text-lg text-orange-950">
+                    More you eat, sooner you dies
+                </p>
+            </div>
+            <img 
+                src="../../public/hero-image2.png"
+                alt="Bolo Zubaan Canceri"
+                width={500}
+                height={500}
+                className="w-[80%] md:w-[93.5%] h-full object-contain sticky top-0 right-0"
+            />
+            </section>
+        </>
+    );
+}
+
+export default Hero;
