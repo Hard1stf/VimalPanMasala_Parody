a6bde60d32c2ed035ac0544691e3ecd75df4d2b0
Hard1stf <hardikvijeta2@gmail.com>
2026-05-11 11:35:16 +0000

    feat(Navbar): Make Navbar working applying SPA principal using Link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,12 +9,12 @@ const NavBar = () => {
                     <h1 className="text-2xl font-extrabold text-orange-600 tracking-tighter">Vimal</h1>
                 </Link>
                 <div className="flex items-center gap-4">
-                    <Link to='#products' className="text-orange-950 font-bold tracking-tight hover:text-orange-900 transition-all duration-300">
+                    <Link to='/products' className="text-orange-950 font-bold tracking-tight hover:text-orange-900 transition-all duration-300">
                         Products
                     </Link>
-                    <button className="bg-orange-600 text-white font-medium px-4 py-2 hover:bg-orange-700 rounded-lg transition-all duration-300">
+                    <Link to='/login' className="bg-orange-600 text-white font-medium px-4 py-2 hover:bg-orange-700 rounded-lg transition-all duration-300">
                         Login
-                    </button>
+                    </Link>
                 </div>
             </nav>
         </>
